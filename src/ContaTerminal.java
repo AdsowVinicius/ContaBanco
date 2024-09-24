9a9c01f5517c9652f8ac5a53d4efbac529ed8079
Adsow Vinicius Freitas Batista <adsow007@gmail.com>
2024-09-24 15:41:12 -0300

    inicio

---
+++ src/ContaTerminal.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Boa tarde, bem vindo ao cadastro do banco lalaBan, primeiramente em diga o sue nome:");
+        String nome = scanner.nextLine();
+        scanner.close();
+        
+
+    }
+}
