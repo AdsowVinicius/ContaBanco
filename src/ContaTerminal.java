fe1772052bc28f10d9f87851900afb8d82919e59
Adsow Vinicius Freitas Batista <adsow007@gmail.com>
2024-09-24 17:10:01 -0300

    app finalizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -6,8 +6,22 @@ public class ContaTerminal {
 
         System.out.println("Boa tarde, bem vindo ao cadastro do banco lalaBan, primeiramente em diga o sue nome:");
         String nome = scanner.nextLine();
+
+        System.out.println("Por favor digite o numero da sua conta:");
+        int numero = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Por favor digite o numero da sua agencia:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor digite o saldo que sera disponibilizado nesta conta:");
+        float saldo = scanner.nextFloat();
+
+        System.out.println("Ola " +nome+ ", obrigado por criar conta no nosso banco, sua agencia é "+agencia+", conta " +numero+ "e seu saldo " +saldo+ " ja esta disponivel !!");
+
         scanner.close();
-        
+
 
     }
 }
